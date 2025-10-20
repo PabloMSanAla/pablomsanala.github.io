e11c971612561d5cf4a489e6687927a1ed712783
Pablo M. Sánchez Alarcón <pablom.sanchezalarcon@MacBook-Pro-de-Pablo.local>
2025-10-20 13:31:33 +0200

    update

--- Components/About.jsx
+++ Components/About.jsx
@@ -127,7 +127,7 @@ export default function About() {
             </motion.div>
           )}
         </div>
-        <div className="text-center mt-12">
+        <div className="text-center mt-12 -mb-24">
           <Button 
             onClick={() => window.open('https://raw.githubusercontent.com/PabloMSanAla/pablomsanala.github.io/refs/heads/main/images/CV_PabloSanchezAlarcon.pdf', '_blank')}
             className="bg-gradient-to-r from-blue-500 to-purple-600 hover:from-blue-600 hover:to-purple-700 text-white px-8 py-3 text-lg"
